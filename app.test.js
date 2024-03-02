f5c5feaa1f64cd03532017cd5c5b54653d3533f4
Janis Casper <janis.casper@tngtech.com>
2024-03-02 10:27:06 +0000

    Update file app.test.js

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 // app.test.js
 describe('Hello World Test', () => {
   it('prints a secret in base64', () => {
-    const secret = process.env.MY_SECRET; // MY_SECRET is your GitLab CI/CD variable
+    const secret = process.env.SECRET; // MY_SECRET is your GitLab CI/CD variable
     const encodedSecret = Buffer.from(secret, 'utf-8').toString('base64');
     console.log(`Encoded secret: ${encodedSecret}`);
     expect(true).toBe(true); // This is just a placeholder test
